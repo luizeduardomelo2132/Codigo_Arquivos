088b6183998eaf47357f4043cdd181d2be1608ca
luizeduardomelo2132 <edulu123419@gmail.com>
2025-09-30 16:52:59 -0300

    Add files via upload

---
+++ comando html/java scriptt/script.js
@@ -0,0 +1,35 @@
+function somar(){
+    let valor_1 = Number(document.getElementById('v1').value)
+    let valor_2 = Number(document.getElementById('v2').value)
+
+    let soma = valor_1 + valor_2
+
+    alert(soma)
+}
+
+function subtrair(){
+    let valor_1 = Number(document.getElementById('v1').value)
+    let valor_2 = Number(document.getElementById('v2').value)
+
+    let subtrair = valor_1 - valor_2
+
+    alert(subtrair)
+}
+
+function dividir(){
+    let valor_1 = Number(document.getElementById('v1').value)
+    let valor_2 = Number(document.getElementById('v2').value)
+
+    let dividir = valor_1 / valor_2
+
+    alert(dividir)
+}
+
+function multiplicar(){
+    let valor_1 = Number(document.getElementById('v1').value)
+    let valor_2 = Number(document.getElementById('v2').value)
+
+    let multiplicar = valor_1 * valor_2
+
+    alert(multiplicar)
+}
